a34a1715f1714591b63b101785f147f985619663
FHuppertz <huppertzfabian@gmail.com>
2025-09-07 11:53:02 +0200

    Made the plotting loop over all Tasks

--- plot_reults.py
+++ plot_reults.py
@@ -1,16 +1,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv("results.csv").groupby("Task")
+task_results = pd.read_csv("results.csv").groupby("Task")
 
-task_group = df.get_group(2)
+# Plot the beleif vs truth for each Task and every experiment in the tasks
+for i in range(len(task_results.groups)):
+    task_group = task_results.get_group(i+1)
+    task_group[["Experiment","belief", "truth"]].plot(x="Experiment", kind="line", linestyle="dotted", marker="o", title=f"Belief values for Task {i+1}")
 
-print(task_group)
 
-task_group[["Experiment","belief", "truth"]].plot(x="Experiment", kind="line", marker="o", title="Belief values for Task 2")
-
-
-mean_toolcalls = df.mean(numeric_only=True)
+# Plotting the mean of all toolcalls for each task
+mean_toolcalls = task_results.mean(numeric_only=True)
 mean_toolcalls.plot(y="Toolcalls", kind="bar", title="Mean Toolcalls of Evaluation")
 plt.legend(["Toolcalls"])
+
+
 plt.show()
